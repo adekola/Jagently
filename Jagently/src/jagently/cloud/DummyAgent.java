224631d5064768a4f955baeb90b09a60ad6a8357
ameerah <ameerah@ameerah-VPCEA15FG>
2015-10-24 18:01:19 +0200

    Having a shot at creating remote agents from code (via cmd line)

---
+++ Jagently/src/jagently/cloud/DummyAgent.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jagently.cloud;
+
+import jade.core.Agent;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ *
+ * @author ameerah
+ */
+public class DummyAgent extends Agent {
+    
+    
+   public void setup(){
+   
+   System.out.println("Hello I dont do much here anyways");
+   }
+    
+    
+}
--- Jagently/src/jagently/cloud/RemoteAgent.java
+++ Jagently/src/jagently/cloud/RemoteAgent.java
@@ -49,44 +49,9 @@ public class RemoteAgent {
             e.printStackTrace();
         }
 
-		//return output.toString();
+		
     }
 
-               //Profile pClient = new ProfileImpl(false);
-    //pClient.setParameter(Profile.MAIN_HOST, "172.26.190.91");
-    //pClient.setParameter(Profile.MAIN_PORT, "1099");
-            /*try {
-
-     Runtime rtClient = Runtime.instance();
-                
-                
-     Profile pClient = new ProfileImpl(false);//("52.30.231.116", 1099, null, false);
-                
-                
-     pClient.setParameter(ProfileImpl.PLATFORM_ID, "platform"+1); 
-     // ID range from 1 to 10 
-     pClient.setParameter(Profile.MAIN_HOST, "52.30.231.116");
-     pClient.setParameter(Profile.MAIN_PORT, "1099"); // available port 
-     pClient.setParameter(Profile.LOCAL_HOST, "jicp://172.30.0.79");
-     pClient.setParameter(Profile.LOCAL_PORT, "1099");
-     rtClient.instance().setCloseVM(false);
-                
-                
-     //ContainerController mc = Runtime.instance().createMainContainer(pClient);
-
-     pClient.setParameter(Profile.CONTAINER_NAME, "newContainer");
-
-     AgentContainer ccAg = (AgentContainer) rtClient.createAgentContainer(pClient);
-
-     AgentController t2 = ccAg.createNewAgent(String.format("%s:%s", "SupervisorAgent", agentCount++), SupervisorAgent.class.getName(), null);
-
-     t2.start();
-
-     //controller=ccAg.createNewAgent("PawnAgent",PawnAgent.class.getName(),Object[] args);
-     //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-     } catch (StaleProxyException ex) {
-     Logger.getLogger(RemoteAgent.class.getName()).log(Level.SEVERE, null, ex);
-     }
-     }
-     */
+     
+     
 }
